1e14e9f6ebe12a429511c7ffebd2ee88f2a4c5f4
krystal <892184375@qq.com>
2018-04-04 01:33:05 +0800

    分模块

---
+++ src/js/song-form.js
@@ -0,0 +1,43 @@
+{
+    let view = {
+        el:'.page > main',
+        template:`
+        <h1>新建歌曲</h1>
+            <form class="form">
+                <div class="row">
+                    <label>
+                        歌名
+                    </label>
+                    <input type="text">
+                </div>
+                <div class="row">
+                    <label>
+                        歌手
+                    </label>
+                    <input type="text">
+                </div>
+                <div class="row">
+                    <label>
+                        外链
+                    </label>
+                    <input type="text">
+                </div>
+                <div class="row actions">
+                    <button type="submit">保存</button>
+                </div>
+            </form>
+        `,
+        render(data){
+            $(this.el).html(this.template) //把内容放进容器里
+        }
+    }
+    let model = {}
+    let controller = {
+        init(view,model){
+            this.view = view
+            this.model = model
+            this.view.render(this.model.data)
+        }
+    }
+    controller.init(view,model)
+}
